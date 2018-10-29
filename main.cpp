407429bebfcaf78d6c277774cb556f68a315cab5
sarthak0906 <sarthak0906@gmail.com>
2018-10-29 21:00:07 +0530

    main update

--- main.cpp
+++ main.cpp
@@ -2,8 +2,88 @@
 
 using namespace std;
 
+typedef long long int ll;
+
+struct node{
+	int x;
+	int y;
+	struct node * next;
+	struct node * prev;	
+}start,last;
+
+typedef struct node * node;
+
+node newnode(int x, int y){
+	node temp = (node)malloc(sizeof(node));
+	temp->x = x;
+	temp->y = y;
+	temp->next = NULL;
+	temp->prev = NULL;
+	return temp;
+}
+
+void moveup(){
+	node temp = newnode(start->x,start->y+1);
+	temp->next = start;
+	start->prev = temp;
+	start = temp;
+	last = last->prev;
+	last->next = NULL;
+	free(last->next);
+}
+
+void movedown(){
+	node temp = newnode(start->x,start->y-1);
+	temp->next = start;
+	start->prev = temp;
+	start = temp;
+	last = last->prev;
+	last->next = NULL;
+	free(last->next);
+}
+
+void moveleft(){
+	node temp = newnode(start->x-1,start->y);
+	temp->next = start;
+	start->prev = temp;
+	start = temp;
+	last = last->prev;
+	last->next = NULL;
+	free(last->next);
+}
+
+void moveright(){
+	node temp = newnode(start->x+1,start->y);
+	temp->next = start;
+	start->prev = temp;
+	start = temp;
+	last = last->prev;
+	last->next = NULL;
+	free(last->next);
+}
+
 int main(){
-	
+	char c;
+	cin >> c; // input char for input 		taking wasd control method
+	while (1){
+		if (kbhit()){ // checking if any key was hitted by the user
+			c = getch();
+		}
+		start->y;
+		start->x;
+		if (int(c) == 27) break; // checking if the key hitted was esc
+		if (c == 'w') moveup();
+		else if (c == 'a') moveleft();
+		else if (c == 's') movedown();
+		else if (c == 'd') moveright();
+		
+		if (start->x == fruitx && start->y == fruity){
+				node temp = newnode(start->x+1,start->y);
+				temp->next = start;
+				start->prev = temp;
+				start = temp;
+		}
+	}
 	return 0;
 }
 
