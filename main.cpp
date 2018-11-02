a5f17b5a520d7a7db3a6f0ca45f5301ebc93cd5f
sarthak0906 <sarthak0906@gmail.com>
2018-11-02 18:35:43 +0530

    some changes in structure

--- main.cpp
+++ main.cpp
@@ -1,35 +1,19 @@
-#include<bits/stdc++.h>
+#include<math.h>
 #include<conio.h>
 #include<graphics.h>
 #include<stdlib.h>
 #include<stdio.h>
 
-using namespace std;
-
-typedef long long int ll;
-
 struct node1{
 	int x;
 	int y;
 	struct node1 * next;
-	struct node1 * prev;	
+	struct node1 * prev;
 }*start,*last;
 
-typedef struct node1 * node;
+int arr[150][150] = {{0}};
 
-int arr[600][600] = {{0}};
-for (int j=0;j<600;++j){
-	arr[0][j] = 0;
-	arr[599][j] = 0;
-	putpixel(0,j,black);
-	putpixel(599,j,black);
-}
-for (int j=0;j<600;++j){
-	arr[j][0] = 0;
-	arr[j][599] = 0;
-	putpixel(j,0,black);
-	putpixel(j,599,black);
-}
+typedef struct node1 * node;
 
 node newnode(int x, int y){
 	node temp = (node)malloc(sizeof(node));
@@ -41,23 +25,28 @@ node newnode(int x, int y){
 }
 
 void startgame(){
-	start = NULL;
-	start = newnode(250,250);
+	int j;
 	node temp = start;
-	while (6--){
+	node t;
+	start = NULL;
+	start = newnode(150,150);
+	for (j=0;j<6;j++){
 		putpixel(temp->x,temp->y,WHITE);
 		arr[temp->x][temp->y] = 1;
 		t = newnode(temp->x,temp->y + 1);
 		temp->next = t;
 		t->prev = temp;
 		temp = t;
+		last = temp;
 	}
 	putpixel(temp->x,temp->y,WHITE);
 	arr[temp->x][temp->y] = 1;
+	last = temp;
 }
 
 void moveup(){
 	node temp = newnode(start->x,start->y+1);
+
 	temp->next = start;
 	start->prev = temp;
 	start = temp;
@@ -76,7 +65,7 @@ void movedown(){
 	start->prev = temp;
 	start = temp;
 	putpixel(start->x,start->y,WHITE);
-	putpixel(last->x,last->y,BLACK);	
+	putpixel(last->x,last->y,BLACK);
 	arr[start->x][start->y] = 0;
 	arr[last->x][last->y] = 0;
 	last = last->prev;
@@ -113,22 +102,41 @@ void moveright(){
 }
 
 int main(){
-	]
+	//int j;
+	int gdriver = DETECT, gmode, errorcode;
+	int gd = DETECT, gm,j;
 	char c;
-	cin >> c; // input char for input 		taking w-a-s-d control method
+	//scanf ("%c",&c);
+	//initgraph(&gdriver, &gmode, "");
+	initgraph(&gd,&gm,"C://TURBOC3//BGI");
+	for (j=0;j<150;j++){
+		arr[0][j] = 0;
+		arr[149][j] = 0;
+		putpixel(0,j,WHITE);
+		putpixel(149,j,WHITE);
+	}
+	getch();
+	for (j=0;j<150;j++){
+		arr[j][0] = 0;
+		arr[j][149] = 0;
+		putpixel(j,0,BLACK);
+		putpixel(j,149,BLACK);
+	}
+	//scanf("%c",&c); // input char for input 		taking w-a-s-d control method
+	c=getch();
 	startgame();
 	while (1){
 		if (kbhit()){ // checking if any key was hitted by the user
 			c = getch();
 		}
-		start->y;
-		start->x;
-		if (int(c) == 27) break; // checking if the key hitted was esc
+	   //	start->y;
+	   //	start->x;
+		if ((int) c == 27) break; // checking if the key hitted was esc
 		if (c == 'w') moveup();
 		else if (c == 'a') moveleft();
 		else if (c == 's') movedown();
 		else if (c == 'd') moveright();
-		
+
 //		if (start->x == fruitx && start->y == fruity){
 //				node temp = newnode(start->x+1,start->y);
 //				temp->next = start;
@@ -136,6 +144,7 @@ int main(){
 //				start = temp;
 //		}
 	}
+	closegraph();
 	return 0;
 }
 
